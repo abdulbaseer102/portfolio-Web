6636e17ac0a1bb7a0eeb3c50643e131e63f58913
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2024-06-16 16:01:03 +0500

    first commit

--- app/components/about/page.tsx
+++ app/components/about/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { motion } from 'framer-motion';
 import React from 'react';
 import { HiAcademicCap, HiCode, HiStar, HiUserGroup } from 'react-icons/hi';
--- app/components/project/page.tsx
+++ app/components/project/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { motion } from 'framer-motion'
 import React from 'react'
 import { HiOutlineLocationMarker } from 'react-icons/hi'
